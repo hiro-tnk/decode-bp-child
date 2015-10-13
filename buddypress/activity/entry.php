637403d6e80ad5463f61d61506824024fcbfa880
田中宏明 <hiro@tanakahiroaki-no-MacBook-Pro.local>
2015-10-13 23:49:42 +0900

    change not to show labels in mobile

--- buddypress/activity/entry.php
+++ buddypress/activity/entry.php
@@ -69,7 +69,8 @@ do_action( 'bp_before_activity_entry' ); ?>
 
 				<?php if ( bp_activity_can_comment() ) : ?>
 
-					<a href="<?php bp_activity_comment_link(); ?>" class="button acomment-reply bp-primary-action" id="acomment-comment-<?php bp_activity_id(); ?>"><?php printf( __( 'Comment <span>%s</span>', 'buddypress' ), bp_activity_get_comment_count() ); ?></a>
+                    <a href="<?php bp_activity_comment_link(); ?>" class="button acomment-reply bp-primary-action" id="acomment-comment-<?php bp_activity_id(); ?>">
+                    <?php if(wp_is_mobile()){printf( __( '%s comment', 'buddypress' ), bp_activity_get_comment_count() );}else{printf( __( '%s', 'buddypress' ), bp_activity_get_comment_count() );} ?></a>
 
 				<?php endif; ?>
 
@@ -77,11 +78,11 @@ do_action( 'bp_before_activity_entry' ); ?>
 
 					<?php if ( !bp_get_activity_is_favorite() ) : ?>
 
-						<a href="<?php bp_activity_favorite_link(); ?>" class="button fav bp-secondary-action" title="<?php esc_attr_e( 'Mark as Favorite', 'buddypress' ); ?>"><?php _e( 'Favorite', 'buddypress' ); ?></a>
+						<a href="<?php bp_activity_favorite_link(); ?>" class="button fav bp-secondary-action" title="<?php esc_attr_e( 'Mark as Favorite', 'buddypress' ); ?>"><?php if(!wp_is_mobile()){_e( 'Favorite', 'buddypress' );} ?></a>
 
 					<?php else : ?>
 
-						<a href="<?php bp_activity_unfavorite_link(); ?>" class="button unfav bp-secondary-action" title="<?php esc_attr_e( 'Remove Favorite', 'buddypress' ); ?>"><?php _e( 'Remove Favorite', 'buddypress' ); ?></a>
+						<a href="<?php bp_activity_unfavorite_link(); ?>" class="button unfav bp-secondary-action" title="<?php esc_attr_e( 'Remove Favorite', 'buddypress' ); ?>"><?php if(!wp_is_mobile()){_e( 'Unfavorite', 'buddypress' );} ?></a>
 
 					<?php endif; ?>
 
